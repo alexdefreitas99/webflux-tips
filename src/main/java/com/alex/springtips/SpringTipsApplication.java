3a5f3436f921f79eacab08c3d4e459a232b89ab2
Alex de Freitas <alex_freitas@sicredi.com.br>
2021-01-18 10:04:24 -0300

    first commit

---
+++ src/main/java/com/alex/springtips/SpringTipsApplication.java
@@ -0,0 +1,13 @@
+package com.alex.springtips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTipsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alex/springtips/SpringTipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.springtips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
